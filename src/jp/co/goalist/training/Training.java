9132b80a6f6fa85823291c3078e8283f9dfeb217
FUYUKI ONO <FUYUKI ONO>
2017-05-10 16:31:50 +0900

    今日の日付を表示する機能を作成

--- src/jp/co/goalist/training/Training.java
+++ src/jp/co/goalist/training/Training.java
@@ -1,5 +1,15 @@
 package jp.co.goalist.training;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 public class Training {
 
+	public static void main(String[] arg) {
+		Calendar cal = Calendar.getInstance();
+		DateFormat df = new SimpleDateFormat("M月d日");
+		String dateStr = df.format(cal.getTime());
+		System.out.println("今日の日付は" + dateStr + "です。");
+	}
 }
